9d01afd6a9ce74710f46f53b8779927215b36ec1
Sankaraleti <sankaraleti143@gmail.com>
2026-03-29 17:02:57 +0530

    feat: implmented curd operations for post

--- schemas.py
+++ schemas.py
@@ -12,16 +12,26 @@ class UserResponse(UserBase):
     id: int
     image_file: str | None
     image_path: str
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None , min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None,min_length=1, max_length=200) 
 class PostBase(BaseModel):
-    title: str = Field(min_length=1 , max_length=100)
-    content: str = Field(min_length=1)
+    title: str | None = Field(default=None ,min_length=1 , max_length=100)
+    content: str | None = Field( default= None ,min_length=1)
     
 
 class PostCreate(PostBase):
     user_id: int
 
+class PostUpdate(PostBase):
+    title: str = Field(min_length=1 , max_length=100)
+
+
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     id: int
+    user_id: int
     date_posted: datetime
     author: UserResponse
